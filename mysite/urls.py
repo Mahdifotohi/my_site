d928b25019b4e33f1b0ccf76c40649bd709690fe
Mahdi Fotohi <mahdifotohi22@gmail.com>
2025-09-04 18:08:12 +0330

    Update static file settings and add CSS for styling

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,8 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', index_view),
+    path('',index_view),
     path('about', about_view),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
